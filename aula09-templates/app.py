0534637151034d1fe56a5e0c381241714e8d3456
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-12 22:37:02 +0000

    Utilizando templates com Flask

--- aula09-templates/app.py
+++ aula09-templates/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('template.html')
+    x = 5
+    y = 10
+    usuarios = {        
+        'George P. Chausse':'713-718-2082',
+        'Livia Ribeiro Barros':'(81) 2753-2153',
+        'Rebeca Costa Oliveira':'(81) 8183-5958',
+        'Júlio Barbosa Sousa':'(31) 2110-9877',
+        'Mariana Alves Costa':'(11) 9306-2119'
+    }
+    return render_template('template.html', x=x, y=y, usuarios=usuarios)
 
 if __name__=="__main__":
     app.run(debug=True)
