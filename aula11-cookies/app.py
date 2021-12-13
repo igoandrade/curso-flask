b82ccd239c6b6e118b13e2832eb26f9d4ecb8f44
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-13 00:40:08 +0000

    aula 11

---
+++ aula11-cookies/app.py
@@ -0,0 +1,13 @@
+import statistics
+from flask import Flask, render_template, request
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def notas():
+    return render_template('index.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
