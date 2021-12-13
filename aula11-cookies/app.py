9e8666aa26e9077536f8950260ad0398b6635114
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-13 01:49:51 +0000

    Aula 11 - Cookies

--- aula11-cookies/app.py
+++ aula11-cookies/app.py
@@ -1,12 +1,25 @@
 import statistics
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
-def notas():
+def index():
     return render_template('index.html')
+
+@app.route('/setcookie', methods=['GET', 'POST'])
+def setcookie():
+    response = make_response(render_template('setcookie.html'))
+    if request.method == 'POST':
+        dados = request.form['cookie']
+        response.set_cookie('testeCookie', dados)
+    return response
+
+@app.route('/getcookie')
+def getcookie():
+    cookieName = request.cookies.get('testeCookie')
+    return render_template('getcookie.html', cookieName=cookieName)
 
 
 if __name__=="__main__":
