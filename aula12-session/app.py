b63d09d92243c2ed707427cf411242c125fb91d2
Igo da Costa Andrade <igoandrade.314@gmail.com>
2021-12-13 03:15:29 +0000

    aula 12

---
+++ aula12-session/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, make_response, url_for, redirect, session
+
+app = Flask(__name__, template_folder='templates', static_folder='public')
+app.secret_key = "123456"
+
+
+@app.route('/')
+def index():
+    username = ''
+    if 'username' in session:
+        username = session['username']
+    return render_template('index.html', username=username)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and request.form['username'] != '':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__=="__main__":
+    app.run(debug=True)
